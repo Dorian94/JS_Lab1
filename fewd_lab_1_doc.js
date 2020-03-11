a4e1714b2926f5c74708866a6a9a3b9d0f9272cf
Dorian94 <dorian.wallez@gmail.com>
2020-03-11 15:31:44 +0000

    commit

---
+++ fewd_lab_1_doc.js
@@ -0,0 +1,42 @@
+/*Dorian WALLEZ
+22490
+https://github.com/Dorian94/JS_Lab1.git */
+
+/*3.*/ var firstName = "Dorian";
+
+/*4.*/ var favouriteColor = "blue";
+
+/*5.
+var city = 'Lubumbashi';
+var country = 'Congo';*/
+
+var favourtieFood = "Chicken Curry";
+var city = "Paris";
+
+/*6. x has the value 'Toe' after the execution.
+
+7. y has the value 'Laurel' after the execution.
+
+/* THis code print firstName,favouriteColor,favourtieFood and city in three different sentence.
+
+8.*/ console.log(firstName,favouriteColor,favourtieFood + " " + city);
+
+/*9. see up here*/ 
+
+/* 10. */ 
+var phrase = "A rainbow is formed by sunlightt. Shining through rain drops so small. The rin bends the light from sunshine, colors the sky for us all.";
+
+var endingPhrase = "is what we see. Rainbow colours, are made with light bending you see!";
+var one = "Red";
+var two = "orange";
+var three = "yellow";
+var four = "green";
+var five = "blue";
+var six = "indigo";
+var seven = "violet";
+
+/* 11. */ console.log(phrase +  one + two + three + four + five + six + seven + endingPhrase); 
+
+
+
+
